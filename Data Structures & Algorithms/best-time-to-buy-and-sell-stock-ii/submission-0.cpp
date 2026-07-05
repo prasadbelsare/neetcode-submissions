891bd04eea8c8c934313440c1d7f99b9d245fe79
Prasad Belsare <57897886+prasadbelsare@users.noreply.github.com>
2026-07-05 15:31:05 -0600

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int buy=prices[0];
+        for(int &p:prices){
+            if(buy<p){
+                profit+=(p-buy);
+            }
+            buy=p;
+        }
+        return profit;
+    }
+};
