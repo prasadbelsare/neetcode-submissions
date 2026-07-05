c054a7c4ecd141cf35c1db6b9743fa6943095cb9
Prasad Belsare <57897886+prasadbelsare@users.noreply.github.com>
2026-07-05 15:13:00 -0600

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=prices[0];
+        int res=0;
+        for(int i=1;i<prices.size();i++){
+            buy=min(buy,prices[i]);
+            res=max(res,prices[i]-buy);
+        }
+        return res;
+    }
+};
