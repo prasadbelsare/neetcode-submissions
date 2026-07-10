abc76fccd1193b07079cd4157910e43a7042680b
Prasad Belsare <57897886+prasadbelsare@users.noreply.github.com>
2026-07-09 22:49:53 -0600

    Add: count-odd-numbers-in-an-interval-range - submission-0

---
+++ Data Structures & Algorithms/count-odd-numbers-in-an-interval-range/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int res=(high-low)/2;
+        if(low%2==1 || high%2==1){
+            res+=1;
+        }
+        return res;
+    }
+};
