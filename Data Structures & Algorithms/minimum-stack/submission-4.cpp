90331c7f2fd794697fbf1d26136997c44eb69faf
Prasad Belsare <57897886+prasadbelsare@users.noreply.github.com>
2026-07-31 13:17:07 -0600

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,26 @@
+class MinStack {
+   private:
+    stack<int> st;
+    stack<int> minStack;
+
+   public:
+    MinStack() {}
+
+    void push(int val) {
+        st.push(val);
+        if (minStack.empty() || minStack.top() >= val) {
+            minStack.push(val);
+        }
+    }
+
+    void pop() {
+        if (st.top() == minStack.top()) {
+            minStack.pop();
+        }
+        st.pop();
+    }
+
+    int top() { return st.top(); }
+
+    int getMin() { return minStack.top(); }
+};
