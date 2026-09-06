0277b3aa0576f0fdb9a2208be51c324931ac83d1
Prasad Belsare <57897886+prasadbelsare@users.noreply.github.com>
2026-09-06 16:14:40 -0600

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>maxHeap;
+        for(int stone:stones){
+            maxHeap.push(stone);
+        }
+        while(maxHeap.size()>1){
+            int y=maxHeap.top();
+            maxHeap.pop();
+            int x=maxHeap.top();
+            maxHeap.pop();
+            if(y>x){
+                maxHeap.push(y-x);
+            }
+        }
+        return maxHeap.empty()?0: maxHeap.top();
+    }
+};
